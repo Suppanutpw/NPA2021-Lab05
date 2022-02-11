c3061eaf720f4056cb053763baa0a9dda2a02ebf
Suppanutpw <54876422+Suppanutpw@users.noreply.github.com>
2022-02-11 15:35:18 +0700

    merge netmiko-pytest beta

--- netmikolab.py
+++ netmikolab.py
@@ -1,3 +1,4 @@
+from distutils import command
 import re
 from netmiko import ConnectHandler
 import json
@@ -32,23 +33,40 @@ def get_ip(device_params, intf):
 def get_subnet(device_params, intf):
     pass
 
-
 def get_description(device_params, intf):
-    data = get_data_from_device(device_params, 'sh int description')
-    result = data.strip().split('\n')
-    for line in result[1:]:
-        # print(line)
-        intf = re.search(r'\s\w+\s\w+', line)
-        print(intf)
+  data = get_data_from_device(device_params, 'sh int description')
+  result = data.strip().split('\n')
+  for line in result[1:]:
+    line += ' '
+    intf_type, intf_num, _, _, description = re.search(r'(\w)\w([0-9/]+)\s+(admin down|\w+)\s+(\w+)([0-9a-zA-Z\s/]+|null)', line).groups()
+    if intf_type == intf[0] and intf_num == intf[1:]:
+      return description.strip()
 
+def get_status(device_params, intf):
+  data = get_data_from_device(device_params, 'sh int description')
+  result = data.strip().split('\n')
+  for line in result[1:]:
+    line += ' '
+    intf_type, intf_num, status, _, _ = re.search(r'(\w)\w([0-9/]+)\s+(admin down|\w+)\s+(\w+)([0-9a-zA-Z\s/]+|null)', line).groups()
+    if intf_type == intf[0] and intf_num == intf[1:]:
+      return status
 
-def set_description():
-    jsonFile = open("testcase.json", "r")
-    routersJson = json.load(jsonFile)
-    jsonFile.close()
+def get_neighbor_link(device_params, intf):
+  data = get_data_from_device(device_params, 'sh cdp nei')
+  result = data.strip().split('\n')
+  for line in result[5:]:
+    try:
+      router_name, lo_intf_type, lo_intf_num, nei_intf_type, nei_intf_num = re.search(r'(\w+).\w+.\w+\s+(\w)\w+\s([0-9/]+)\s+\d+\s+.+?(?=Gig)(\w)\w+\s([0-9/]+)', line).groups()
+      if lo_intf_type == intf[0] and lo_intf_num == intf[1:]:
+        return "Connect to " + nei_intf_type + nei_intf_num + " of " + router_name
+    except:
+      return "Not Use"
 
-    for device in routersJson:
-        with ConnectHandler(**device_params) as ssh:
-            result = ssh.send_command('conf ter')
+def set_description(device_params, intf):
+  commands = ["int " + intf, "description " + get_neighbor_link(device_params, intf)]
+  with ConnectHandler(**device_params) as ssh:
+    result = ssh.send_config_set(commands)
+    print(result)
 
-            result = ssh.send_command('conf ter')
+set_description(device_params, "G0/3")
+print(get_description(device_params, "G0/3"))
