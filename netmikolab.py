1571c46606299dbd2335cab5c3f8b803682efb0b
Suppanutpw <54876422+Suppanutpw@users.noreply.github.com>
2022-02-11 16:48:39 +0700

    .

--- netmikolab.py
+++ netmikolab.py
@@ -1,24 +1,13 @@
-from distutils import command
 import re
-from netmiko import ConnectHandler
 import json
-
-device_ip = "172.31.107.4"
-username = 'admin'
-password = 'cisco'
-device_params = {
-    'device_type': 'cisco_ios',
-    'ip': device_ip,
-    'username': username,
-    'password': password
-}
-
+from netaddr import IPNetwork
+from distutils import command
+from netmiko import ConnectHandler
 
 def get_data_from_device(device_params, command):
     with ConnectHandler(**device_params) as ssh:
         result_shipintbr = ssh.send_command(command)
         return result_shipintbr
-
 
 def get_ip(device_params, intf):
     data = get_data_from_device(device_params, 'sh ip int br')
@@ -29,9 +18,13 @@ def get_ip(device_params, intf):
         if intf_type == intf[0] and intf_num == intf[1:]:
             return intf_ip
 
-
 def get_subnet(device_params, intf):
-    pass
+  data = get_data_from_device(device_params, 'sh int ' + intf)
+  try:
+    cdiraddress = re.search(r'  Internet address is ([0-9./]+)', data).groups()
+    return "%s"%IPNetwork(cdiraddress[0]).netmask
+  except:
+    return "unassigned"
 
 def get_description(device_params, intf):
   data = get_data_from_device(device_params, 'sh int description')
@@ -67,6 +60,3 @@ def set_description(device_params, intf):
   with ConnectHandler(**device_params) as ssh:
     result = ssh.send_config_set(commands)
     print(result)
-
-#set_description(device_params, "G0/3")
-#print(get_description(device_params, "G0/3"))
